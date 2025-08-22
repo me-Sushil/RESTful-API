2cb48fba5e94e79b0befc88ef90185f1ac257121
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-22 21:22:31 +0545

    this is first commit however most of the code already writen in locally

---
+++ server/index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  { id: "1", 
+    name: "Sushil Bishowkarma", 
+    number: "9819909012",
+    important: true
+ },
+  { id: "2", 
+    name: "Akash Tolange", 
+    number: "9719093412",
+    important: true
+},
+  { id: "3", 
+    name: "Niru magar", 
+    number: "9813409018",
+    important: true
+ },
+  { id: "4", 
+    name: "Muskan ....", 
+    number: "9811909019",
+    important: true
+ },
+  { id: "5", 
+    name: "Libina Rai", 
+    number: "9719309011",
+    important: true
+ },
+  { id: "6", 
+    name: "Sirisha .....", 
+    number: "9714609014",
+    important: true
+ }
+];
+
+// GET request   and  HTML  response
+app.get("/", (request, response)=>{
+    response.send(
+        `<div>
+        <p>Hello akash How are you</p>
+        <div>`
+    )
+})
+
+//GET request and JSON response
+app.get("/api/notes", (request, response) => {
+    response.json(notes);
+});
+
+// GET request with Specific ID
+app.get("/api/notes/:noteid", (request, response)=>{
+    const id = request.params.noteid;
+    const findUserSearchId = notes.find(note=> note.id === id);
+    response.json(findUserSearchId);
+})
+
+// DELETE request with Specific ID
+app.delete("/api/notes/:noteid", (request, response)=>{
+    const id = request.params.noteid;
+    notes = notes.filter((note)=>note.id !== id);
+    response.status(204).end()
+
+})
+
+//POST request with body but we handle loosely
+app.post("/api/notes/",(request, response)=>{
+    const data = request.body;
+    data.id= notes.length+1;
+    notes.push(data);
+    response.json(notes);
+})
+
+
+app.post("/api/notes/", (request, response) => {
+   const data = request.body;
+notes.push(data);
+response.json(notes);
+
+});
+
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});
